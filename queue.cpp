bf887a76ab123222ac1d29f4f63c1e00e0465ef6
Md. Tamim khan <mdtamimkhan842@gmail.comgit config --global user.name mdtamimkhan842omit --global to set the identity only in the repository$ git config --global user.email mdtamimkhan842@gmail.com>
2023-05-06 03:44:47 +0600

    second commit

---
+++ queue.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int p, n, k;
+    cin >> p;
+    queue<int> q;
+    while (p--)
+    {
+        cin >> n;
+        if (n > 1)
+        {
+            continue;
+        }
+        else
+        {
+            for (int i = 0; i < n; i++)
+            {
+                cin >> k;
+                q.push(k);
+            }
+        }
+    }
+    q.pop();
+    cout << q.front() << endl;
+
+    while (q.size() > 0)
+    {
+        cout << q.front() << endl;
+        break;
+    }
+}
